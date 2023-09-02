983db9a50cb377983717ca22dfe5cace08e48055
Thabo Diholo <thabo@solvyng.io>
2023-09-02 13:13:25 +0200

    add var foulder

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,8 @@
 def call(Map stageParams){
 
-    checkout{[
+    checkout([
         $class: 'GitSCM',
         branches: [[name: stageParams.branch]],
         userRemoteConfigs: url:stageParams.url
-    ]}
+    ])
 }
